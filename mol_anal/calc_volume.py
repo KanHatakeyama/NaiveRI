5321ebac31acf6269e3cab84f352637ced9df0f2
KanHatakeyama <yaj_k@msn.com>
2022-10-29 18:44:12 +0900

    implement calc volume

--- mol_anal/calc_volume.py
+++ mol_anal/calc_volume.py
@@ -1,6 +1,23 @@
 from .atom_dict import atom_van_dict
 from .mol_utils import get_bond_length
 import numpy as np
+from .mol_utils import get_atom_ids, get_bond_list
+
+
+def calc_mol_volume(mol):
+    conf = mol.GetConformers()[0]
+    atom_ids = get_atom_ids(mol)
+
+    bond_list, repeat_bond = get_bond_list(mol)
+
+    V = 0
+
+    for target_atom_id in atom_ids:
+        dV = calc_dVA(mol, conf, bond_list, target_atom_id, repeat_bond)
+        V += dV
+        # print("dV",target_atom_id,dV)
+
+    return 0.602*V
 
 
 def calc_s(mol, conf, bond, repeat_bond=[]):
@@ -14,7 +31,8 @@ def calc_s(mol, conf, bond, repeat_bond=[]):
     Rj = atom_van_dict[atom2]
 
     if set(repeat_bond) == set((atom_id1, atom_id2)):
-        di = 1.7
+        # const length for repeating bond
+        di = 1.5
     else:
         di = get_bond_length(conf, atom_id1, atom_id2)
 
@@ -26,11 +44,20 @@ def calc_s(mol, conf, bond, repeat_bond=[]):
 def calc_dVA(mol, conf, bond_list, target_atom_id, repeat_bond):
 
     sigma = 0
-    neighbor_atom_bonds = [
+    neighbor_atom_bonds_set = [
         bond for bond in bond_list if target_atom_id in bond]
+
+    # atom order is important!
+
+    neighbor_atom_bonds = []
+    for bond in neighbor_atom_bonds_set:
+        if target_atom_id != bond[0]:
+            bond = bond[1], bond[0]
+        neighbor_atom_bonds.append(bond)
+
     for bond in neighbor_atom_bonds:
         s, Ri = calc_s(mol, conf, bond, repeat_bond)
-        print(s,  bond)
+        #print(s,  bond)
         sigma += s
 
     dVa = 4/3*np.pi*Ri**3-sigma
