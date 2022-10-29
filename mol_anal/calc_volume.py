6fd926cea008a4045d44939b4a90dc9c7ed4cd79
KanHatakeyama <yaj_k@msn.com>
2022-10-29 18:37:20 +0900

    calc

--- mol_anal/calc_volume.py
+++ mol_anal/calc_volume.py
@@ -3,7 +3,7 @@ from .mol_utils import get_bond_length
 import numpy as np
 
 
-def calc_s(mol, conf, bond):
+def calc_s(mol, conf, bond, repeat_bond=[]):
 
     atom_id1 = bond[0]
     atom_id2 = bond[1]
@@ -12,8 +12,26 @@ def calc_s(mol, conf, bond):
 
     Ri = atom_van_dict[atom1]
     Rj = atom_van_dict[atom2]
-    di = get_bond_length(conf, atom_id1, atom_id2)
+
+    if set(repeat_bond) == set((atom_id1, atom_id2)):
+        di = 1.7
+    else:
+        di = get_bond_length(conf, atom_id1, atom_id2)
 
     hi = Ri-(Ri**2+di**2-Rj**2)/(2*di)
     s = 1/3*np.pi*hi**2*(3*Ri-hi)
-    return s
+    return s, Ri
+
+
+def calc_dVA(mol, conf, bond_list, target_atom_id, repeat_bond):
+
+    sigma = 0
+    neighbor_atom_bonds = [
+        bond for bond in bond_list if target_atom_id in bond]
+    for bond in neighbor_atom_bonds:
+        s, Ri = calc_s(mol, conf, bond, repeat_bond)
+        print(s,  bond)
+        sigma += s
+
+    dVa = 4/3*np.pi*Ri**3-sigma
+    return dVa
--- mol_anal/mol_utils.py
+++ mol_anal/mol_utils.py
@@ -24,7 +24,65 @@ def get_bond_length(conf, i, j):
 
 
 def get_bond_list(mol):
+    symbol_id_list = get_symbol_atom_ids(mol)
+
     bond_list = []
+    symbol_bond_list = []
     for bond in mol.GetBonds():
-        bond_list.append((bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()))
-    return bond_list
+        bid1 = bond.GetBeginAtomIdx()
+        bid2 = bond.GetEndAtomIdx()
+
+        # ignore symbol atoms
+        symbol_flag = False
+        for atom_id in (bid1, bid2):
+            if atom_id in symbol_id_list:
+                symbol_flag = True
+
+        if symbol_flag:
+            symbol_bond_list.append((bid1, bid2))
+
+        bond_list.append((bid1, bid2))
+
+    # add repeating unit bond
+    repeat_bond = find_repeat_bond(mol, bond_list)
+    bond_list.append(repeat_bond)
+
+    for b in symbol_bond_list:
+        bond_list.remove(b)
+    return bond_list, repeat_bond
+
+
+def find_repeat_bond(mol, bond_list):
+    symbol_bonds = get_symbol_atom_ids(mol)
+
+    repeat_bond = []
+    for bond in bond_list:
+        for b in bond:
+            if b in symbol_bonds:
+                repeat_bond.extend(bond)
+
+    repeat_bond.remove(symbol_bonds[0])
+    repeat_bond.remove(symbol_bonds[1])
+    return repeat_bond
+
+
+def get_symbol_atom_ids(mol, unit_symbol="*", inverse_mode=False):
+    symbol_list = []
+    normal_atom_list = []
+    atoms = mol.GetNumAtoms()
+    for idx in range(atoms):
+        symbol = mol.GetAtomWithIdx(idx).GetSymbol()
+        if symbol == unit_symbol:
+            symbol_list.append(idx)
+        else:
+            if inverse_mode:
+                normal_atom_list.append(idx)
+
+    if inverse_mode:
+        return normal_atom_list
+
+    return symbol_list
+
+
+def get_atom_ids(mol, unit_symbol="*"):
+    return get_symbol_atom_ids(mol, unit_symbol, inverse_mode=True)
